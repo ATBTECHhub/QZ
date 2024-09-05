30556ed10f50954777710332b12b234fec1a39bd
Jummy007 <gbemisoyeadejumoke@gmail.com>
2024-09-06 00:36:05 +0100

    FEAT: Mobile nav implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 import logo from "../assets/logo.svg";
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <nav className="h-[96px] flex items-center fixed w-full z-[2] bg-white font-pop">
       <div className="container flex justify-between items-center">
@@ -37,7 +43,60 @@ const Navbar = () => {
             Log in
           </button>
         </Link>
-        <IoMenu className="text-2xl lg:hidden"/>
+        <IoMenu className="text-2xl lg:hidden" onClick={handleNav} />
+      </div>
+      <div
+        style={{
+          transition: "all 0.3s ease",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.15)",
+        }}
+        className={
+          nav
+            ? "bg-white fixed z-[4] top-0 right-0 w-full h-screen"
+            : "hidden"
+        }
+      >
+        <div className="flex justify-between items-center px-[15px] py-[20px]">
+          <Link>
+            <img src={logo} alt="/" />
+          </Link>
+          <IoClose className="text-3xl text-primary" onClick={handleNav} />
+        </div>
+        <ul className="text-base font-medium text-darkPrimary">
+          <li className="px-[15px] py-[20px] font-normal leading-[24px] border-y border-gray">
+            <Link to="/" className="leading-4" onClick={handleNav}>
+              Home{" "}
+            </Link>
+          </li>
+
+          <li className="px-[15px] py-[20px] leading-[24px]">
+            <Link to="/features" className="leading-4" onClick={handleNav}>
+              Features
+            </Link>
+          </li>
+
+          <li className="px-[15px] py-[20px] leading-[24px] border-y border-gray">
+            <Link to="/about-us" className="leading-4" onClick={handleNav}>
+              About us
+            </Link>
+          </li>
+
+          <li className="px-[15px] py-[20px] leading-[24px] border-b border-gray">
+            <Link to="/contact-us" className="leading-4" onClick={handleNav}>
+              Contact us
+            </Link>
+          </li>
+          <li className="px-[15px] py-[30px] leading-[24px]">
+            <Link
+              to="/"
+              className="px-[50px] bg-secondary rounded-[30px] py-[10px]
+              text-base font-medium shadow-btnShadow"
+              onClick={handleNav}
+            >
+              Log in
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
