d536b3fc624e5026976a783b4b632eab59c88146
Jummy007 <gbemisoyeadejumoke@gmail.com>
2024-12-06 00:42:03 +0100

    Authorization issue sorted

--- src/config.js
+++ src/config.js
@@ -1,8 +1,25 @@
-import axios from "axios"
+import axios from "axios";
+import useAuthStore from "./store/authStore";
+// const token = useAuthStore.getState().token;
 export const Axios = axios.create({
   baseURL: "https://qz-platform-backend-1.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
     // "Access-Control-Allow-Origin": "*",
+    // Authorization: `Bearer ${token}`,
   },
-}); 
+});
+
+// Add a request interceptor to include the token dynamically
+Axios.interceptors.request.use(
+  (config) => {
+    const token = useAuthStore.getState().token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
