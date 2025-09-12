2fe4d7731d589fccd71e21b3827a65c34211fb54
ATBTECHhub <atbtechhub@gmail.com>
2025-09-12 15:57:13 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['*'],
-  }
+    // allow specific host
+    allowedHosts: ['qz-vht0.onrender.com'],
+  },
 })
